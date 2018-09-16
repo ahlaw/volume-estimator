6583dcdbf07b57fa40c4bf8a72d4eaf0b7b6a84c
Amos Law <amos.law98@gmail.com>
2018-09-16 02:35:39 -0400

    Improved contour search by reducing background noise

--- volume.py
+++ volume.py
@@ -16,21 +16,21 @@ def main(images):
     imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     imgray = cv2.GaussianBlur(imgray, (7, 7), 0)
 
-    '''
-    edged = cv2.Canny(imgray, 55, 100)
+
+    edged = cv2.Canny(imgray, 30, 100)
     edged = cv2.dilate(edged, None, iterations=1)
     edged = cv2.erode(edged, None, iterations=1)
-    '''
-   
-    ret, thresh = cv2.threshold(imgray, 55, 255, cv2.THRESH_BINARY)
 
-    _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL,
+   
+    #ret, thresh = cv2.threshold(imgray, 55, 255, cv2.THRESH_BINARY)
+
+    _, contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL,
             cv2.CHAIN_APPROX_NONE)
     largest_contours = sorted(contours, key=cv2.contourArea)[-2:]
 
     cv2.drawContours(imgray, largest_contours, -1, (0, 255, 0), 3)
     
-    # Find pixel/mm scale
+    # Find mm / pixel scale
     quarter_diameter = 23.88
     coin_contour = np.vstack(largest_contours[1]).squeeze()
     x_values = [x for x, y in coin_contour]
